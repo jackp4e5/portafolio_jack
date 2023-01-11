de621d39ff9e08fb3631517d39a6cee0c4e023a2
Jack pachon <jack.kilian0196@gmail.com>
2023-01-11 17:18:12 -0500

    javascript changes '

--- docs/index.js
+++ docs/index.js
@@ -12,8 +12,14 @@ const ubucacion = element4.getBoundingClientRect();
 
 const trabajos = document.querySelector(".trabajos");
 const about = document.querySelector(".about");
+const inicio = document.querySelector(".inicio");
 let scroll;
 
+const href = (string) => {
+  hamburger.addEventListener("click", () => {
+    hamburger.href = `index.html#${string}`;
+  });
+};
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
   close.classList.toggle("active");
@@ -24,20 +30,31 @@ window.addEventListener("scroll", () => {
   scroll = Number(window.scrollY.toFixed(0));
   if (scroll > 1100 && scroll < 1350) {
     element4.classList.add("transform-y-2");
+    console.log('elemeneto 4');
+    href("About");
   } else if (scroll > 1000 && scroll < 1150) {
     element3.classList.add("transform-x");
+    href("About");
   } else if (scroll > 450 && scroll < 500) {
     element2.classList.add("transform-y");
+    href("Trabajos");
   } else if (scroll > 360 && scroll < 450) {
     element1.classList.add("aparecer2");
+    href("Trabajos");
   }
 });
 
+inicio.addEventListener("click", () => {
+  href("Inicio");
+});
+
 trabajos.addEventListener("click", () => {
+  href("Trabajos");
   element1.classList.add("aparecer2");
   element2.classList.add("transform-y");
 });
 about.addEventListener("click", () => {
+  href("About");
   element1.classList.add("aparecer2");
   element2.classList.add("transform-y");
   element3.classList.add("transform-x");
